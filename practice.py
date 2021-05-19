7c5eb5dff3f14f2a6abc2bc15cc26d9f4f024af5
DKrumpl1 <bakdavid0420@gmail.com>
2021-05-19 14:07:29 +0200

    max fuggeveny

--- practice.py
+++ practice.py
@@ -1,1 +1,11 @@
-print("csumi")
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def main():
+    print("Hello World!")
+    print(max(5, 6))
+if __name__ == " main ":
+    main()
